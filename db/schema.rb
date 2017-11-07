eda2d99aea616f2cb52fb909048c41f7c780e50f
Ubuntu <vagrant@Base-Box>
2017-11-07 22:39:45 +0000

    Added Province model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107051139) do
+ActiveRecord::Schema.define(version: 20171107223529) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20171107051139) do
     t.string "email_address"
     t.string "image"
     t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
