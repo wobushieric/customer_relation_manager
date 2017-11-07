4057a752a74a403c3dfe658c00632eb57190756f
Ubuntu <vagrant@Base-Box>
2017-11-07 15:54:33 +0000

    Added customers controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,10 @@
+class CustomersController < ApplicationController
+  def index
+  end
+
+  def alphabetized
+  end
+
+  def missing_email
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
+  get 'customers/alphabetized'
+
+  get 'customers/missing_email'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
